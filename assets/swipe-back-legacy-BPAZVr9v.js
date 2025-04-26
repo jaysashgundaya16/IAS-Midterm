aee10ce7fc02b81f2204bac52ff273365c225cc6
GundayaFarah <farahgundaya123@gmail.com>
2025-04-26 17:55:09 +0800

    Updates

--- assets/swipe-back-legacy-BPAZVr9v.js
+++ assets/swipe-back-legacy-BPAZVr9v.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CkZgyldm.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-AiUYmVwx.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
